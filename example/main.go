4c551b189b0a7f8cea27a59a8e3743de4689bbad
Kris Foster <kris.foster@gmail.com>
2017-01-05 21:58:30 -0800

    initial commit

---
+++ example/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/transitorykris/v6match"
+)
+
+func main() {
+	fmt.Println(v6match.MatchIPv6("This 1:2:3::4 is an IPv6 address")) // [1:2:3::4]
+	fmt.Println(v6match.MatchIPv6("1:2:3::4, 5:6:7::/32, ::/0"))       // [1:2:3::4 5:6:7::/32 ::/0]
+	fmt.Println(v6match.MatchIPv6("192.168.1.1 is not a v6 address"))  // []
+}
---
+++ main_test.go
@@ -0,0 +1,50 @@
+package v6match
+
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestTrimV6(t *testing.T) {
+	Convey("When checking if a rune is an IPv6 character", t, func() {
+		Convey("And it is", func() {
+			const chars = "abcdefABCDEF0123456789:"
+			for _, c := range chars {
+				So(TrimV6(c), ShouldBeFalse)
+			}
+		})
+		Convey("And it isn't", func() {
+			// We'll just check a few...
+			const chars = "!@#$/QWRTYzxv,"
+			for _, c := range chars {
+				So(TrimV6(c), ShouldBeTrue)
+			}
+		})
+	})
+}
+
+func TestMatchIPv6(t *testing.T) {
+	Convey("When looking for IPv6 addresses in a string", t, func() {
+		Convey("And it does not contain one", func() {
+			matches := MatchIPv6("This does not contain an IPv6 address")
+			So(len(matches), ShouldEqual, 0)
+		})
+		Convey("And it is an IPv6 address only", func() {
+			matches := MatchIPv6("1:2:3::4")
+			So(len(matches), ShouldEqual, 1)
+			So(matches[0], ShouldEqual, "1:2:3::4")
+		})
+		Convey("And it contains two in a sentence", func() {
+			matches := MatchIPv6("1:2:3::4, and 2001::/32 are cool")
+			So(len(matches), ShouldEqual, 2)
+			So(matches[0], ShouldEqual, "1:2:3::4")
+			So(matches[1], ShouldEqual, "2001::/32")
+		})
+		Convey("And it contains an obfuscated address", func() {
+			matches := MatchIPv6("1[:]2{:}3(::)4")
+			So(len(matches), ShouldEqual, 1)
+			So(matches[0], ShouldEqual, "1:2:3::4")
+		})
+	})
+}
