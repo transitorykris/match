bd54d9dc3b1af4d0f67ae2aaac93d6bf68219784
Kris Foster <kris.foster@gmail.com>
2017-01-07 22:17:07 -0800

    update code and tests to reflect that this works for v6 and v4

--- example/example.go
+++ example/example.go
@@ -7,7 +7,9 @@ import (
 )
 
 func main() {
-	fmt.Println(v6match.MatchIPv6("This 1:2:3::4 is an IPv6 address")) // [1:2:3::4]
-	fmt.Println(v6match.MatchIPv6("1:2:3::4, 5:6:7::/32, ::/0"))       // [1:2:3::4 5:6:7::/32 ::/0]
-	fmt.Println(v6match.MatchIPv6("192.168.1.1 is not a v6 address"))  // []
+	fmt.Println(v6match.MatchIP("This string has 1.2 4:5 no IPs in it")) // []
+	fmt.Println(v6match.MatchIP("This 1:2:3::4 is an IPv6 address"))     // [1:2:3::4]
+	fmt.Println(v6match.MatchIP("1:2:3::4, 5:6:7::/32, ::/0"))           // [1:2:3::4 5:6:7::/32 ::/0]
+	fmt.Println(v6match.MatchIP("192.168.1.1 is not a v6 address"))      // [192.168.1.1]
+	fmt.Println(v6match.MatchIP("192.168.1.0/24 is a fine CIDR"))        // [192.168.1.0/24]
 }
--- v6match.go
+++ v6match.go
@@ -5,11 +5,11 @@ import (
 	"strings"
 )
 
-const v6chars = "abcdefABCDEF0123456789:"
+const validRunes = "abcdefABCDEF0123456789:"
 
-// TrimV6 will trim non-v6 characters when using strings.TrimFunc
-func TrimV6(r rune) bool {
-	if strings.ContainsRune(v6chars, r) {
+// TrimAddress will trim non-v4 and v6 characters when using strings.TrimFunc
+func TrimAddress(r rune) bool {
+	if strings.ContainsRune(validRunes, r) {
 		return false
 	}
 	return true
@@ -25,14 +25,14 @@ func stripBrackets(s string) string {
 	return clean.Replace(s)
 }
 
-// MatchIPv6 will return a slice of all IPv6 addresses found in a string
-func MatchIPv6(s string) []string {
+// MatchIP will return a slice of all IP addresses found in a string
+func MatchIP(s string) []string {
 	s = stripBrackets(s)
 	var matches []string
 	// We're going to check each token in the string
 	parts := strings.Split(s, " ")
 	for _, part := range parts {
-		part := strings.TrimFunc(part, TrimV6)
+		part := strings.TrimFunc(part, TrimAddress)
 		var match string
 		// Need to treat parsing CIDRs differently from IPs without lengths
 		if strings.Contains(part, "/") {
