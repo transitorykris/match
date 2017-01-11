ae4bd2245790bd9ec3185fc70eda19b255e12d51
Kris Foster <kris.foster@gmail.com>
2017-01-10 19:35:31 -0800

    show v6 and v4 together in example

--- example/example.go
+++ example/example.go
@@ -7,9 +7,9 @@ import (
 )
 
 func main() {
-	fmt.Println(match.IP("This string has 1.2 4:5 no IPs in it")) // []
-	fmt.Println(match.IP("This 1:2:3::4 is an IPv6 address"))     // [1:2:3::4]
-	fmt.Println(match.IP("1:2:3::4, 5:6:7::/32, ::/0"))           // [1:2:3::4 5:6:7::/32 ::/0]
-	fmt.Println(match.IP("192.168.1.1 is not a v6 address"))      // [192.168.1.1]
-	fmt.Println(match.IP("192.168.1.0/24 is a fine CIDR"))        // [192.168.1.0/24]
+	fmt.Println(match.IP("This string has 1.2 4:5 no IPs in it"))    // []
+	fmt.Println(match.IP("This 1:2:3::4 is an IPv6 address"))        // [1:2:3::4]
+	fmt.Println(match.IP("1:2:3::4, 5:6:7::/32, ::/0"))              // [1:2:3::4 5:6:7::/32 ::/0]
+	fmt.Println(match.IP("192.168.1.1 is not v6, 2001:123::/32 is")) // [192.168.1.1 2001:123::/32]
+	fmt.Println(match.IP("192.168.1.0/24 is a fine CIDR"))           // [192.168.1.0/24]
 }
